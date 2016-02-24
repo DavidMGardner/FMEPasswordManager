705b803b751790309de86ac0d09823e7a0f743fc
Dave Gardner <dave@texasgardners.com>
2016-02-23 21:55:11 -0600

    have posting password entity and get all working in web api

---
+++ src/FMEPasswordManager.Api/App_Start/IoC.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using FME.PasswordManager;
+using Serilog;
+using Serilog.Core;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace FMEPasswordManager.Api.App_Start
+{
+    public static class IoC
+    {
+        private static readonly Lazy<Container> ContainerBuilder = new Lazy<Container>(DefaultContainer, LazyThreadSafetyMode.ExecutionAndPublication);
+
+        public static IContainer Container => ContainerBuilder.Value;
+
+        private static Container DefaultContainer()
+        {
+            var levelSwitch = new LoggingLevelSwitch();
+
+            var log = new LoggerConfiguration()
+              .MinimumLevel.ControlledBy(levelSwitch)
+              .WriteTo.ColoredConsole()
+              .CreateLogger();
+
+            var container = new Container();    
+            container.ForObject(log);
+
+            container.Configure(x =>
+            {
+                x.Scan(scan =>
+                {
+                    scan.TheCallingAssembly();
+                    scan.WithDefaultConventions();
+                });
+                x.For<IEntityPersistence<PasswordEntity>>().Use<JsonFilePersistence<PasswordEntity>>();
+                x.For<IRepository<PasswordEntity>>().Use<JsonRepository<PasswordEntity>>();
+                x.For<IConfiguration>().Use<Configuration>();
+                x.For<IEncryptionStrategy>().Use<TripleDESEncryptionStrategy>();
+                x.For<IPasswordManagement>().Use<PasswordManagement>();
+                x.For<ILogger>().Use(log);
+            });
+
+            return container;
+        }
+    }
+}
---
+++ src/FMEPasswordManager.Api/Controllers/PasswordManagerController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FME.PasswordManager;
+
+namespace FMEPasswordManager.Api.Controllers
+{
+    public class PasswordManagerController : ApiController
+    {
+        private readonly IRepository<PasswordEntity> _repository;
+
+        public PasswordManagerController(IRepository<PasswordEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/PasswordManager
+        [Route("api/PasswordManager")]
+        [HttpPost]
+        public IEnumerable<PasswordEntity> GetFromBody([FromBody]string value)
+        {
+            _repository.MasterKey = value;
+            return _repository.GetAll();
+        }
+
+        // GET: api/PasswordManager/5
+        public PasswordEntity Get(string id)
+        {
+            return _repository.GetById(id);
+        }
+
+        // POST: api/PasswordManager
+        public PasswordEntity Post([FromBody]PasswordManagementArgs value)
+        {
+            _repository.MasterKey = value.MasterKey;
+            return _repository.Insert(value.Entity);
+        }
+
+        // PUT: api/PasswordManager/5
+        public void Put(int id, [FromBody]string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE: api/PasswordManager/5
+        public void Delete(string id)
+        {
+            var aggregateRoot = _repository.GetById(id);
+            if(aggregateRoot != null)
+                _repository.Delete(aggregateRoot);
+        }
+    }
+
+    public class PasswordManagementArgs
+    {
+        public PasswordEntity Entity { get; set; }
+        public string MasterKey { get; set; }
+    }
+}
