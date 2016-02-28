fe0befab04da443a1ec71ca2c7684153b3562e6d
Wes White <wtealwhite@gmail.com>
2016-02-28 11:02:32 -0600

    Finished gulp build automation

--- src/FME.PasswordManager.Web/app/routes.js
+++ src/FME.PasswordManager.Web/app/routes.js
@@ -1,12 +1,10 @@
 ﻿import React from 'react';
 import {Route} from 'react-router';
 import App from './components/App';
-import Home from './components/Home';
 import AddAccount from './components/AddAccount';
 
 export default (
     <Route component={App}>
-        <Route path='/' component={Home} />
         <Route path='/add' component={AddAccount} />
     </Route>
 );
--- src/FME.PasswordManager.Web/gulpfile.js
+++ src/FME.PasswordManager.Web/gulpfile.js
@@ -1,12 +1,12 @@
 ﻿var gulp = require('gulp');
 var gutil = require('gulp-util');
 var gulpif = require('gulp-if');
-var streamify = require('gulp-streamify');
 var autoprefixer = require('gulp-autoprefixer');
 var cssmin = require('gulp-cssmin');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
+var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
 var babelify = require('babelify');
 var browserify = require('browserify');
@@ -24,7 +24,7 @@ var dependencies = [
     'underscore'
 ];
 
-gulp.task('vendor', function () {
+gulp.task('vendor', function() {
     return gulp.src([
         'bower_components/jquery/dist/jquery.js',
         'bower_components/bootstrap/dist/js/bootstrap.js',
@@ -35,51 +35,57 @@ gulp.task('vendor', function () {
         .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('browserify-vendor', function () {
+
+gulp.task('browserify-vendor', function() {
     return browserify()
         .require(dependencies)
         .bundle()
         .pipe(source('vendor.bundle.js'))
-        .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+        .pipe(buffer())
+        .pipe(gulpif(production, uglify({ mangle: false })))
         .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('browserify', ['browserify-vendor'], function () {
+gulp.task('browserify', ['browserify-vendor'], function() {
     return browserify({ entries: 'app/main.js', debug: true })
         .external(dependencies)
         .transform(babelify, { presets: ['es2015', 'react'] })
-        .pipe(streamify(sourcemaps.init({ loadMaps: true })))
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
-        .pipe(streamify(sourcemaps.write('.')))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(gulpif(production, uglify({ mangle: false })))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('browserify-watch', ['browserify-vendor'], function () {
+
+gulp.task('browserify-watch', ['browserify-vendor'], function() {
     var bundler = watchify(browserify({ entries: 'app/main.js', debug: true }, watchify.args));
     bundler.external(dependencies);
-    bundler.transform(babelify, { presets: ['es2015', 'react'] })
+    bundler.transform(babelify, { presets: ['es2015', 'react'] });
     bundler.on('update', rebundle);
     return rebundle();
 
     function rebundle() {
         var start = Date.now();
         return bundler.bundle()
-            .on('error', function (err) {
+            .on('error', function(err) {
                 gutil.log(gutil.colors.red(err.toString()));
             })
-            .on('end', function () {
+            .on('end', function() {
                 gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
             })
             .pipe(source('bundle.js'))
-            .pipe(streamify(sourcemaps.init({ loadMaps: true })))
-            .pipe(streamify(sourcemaps.write('.')))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest('public/js/'));
     }
 });
 
-gulp.task('styles', function () {
+
+gulp.task('styles', function() {
     return gulp.src('app/stylesheets/main.less')
         .pipe(plumber())
         .pipe(less())
@@ -88,7 +94,7 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('app/stylesheets/**/*.less', ['styles']);
 });
 
