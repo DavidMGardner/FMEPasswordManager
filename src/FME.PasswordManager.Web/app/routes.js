1d9a3ee1df587396a1544381f2e4373f921d486e
Wes White <wtealwhite@gmail.com>
2016-02-28 01:49:18 -0600

    smoothing out front end build. Created add account component

--- src/FME.PasswordManager.Web/app/routes.js
+++ src/FME.PasswordManager.Web/app/routes.js
@@ -1,1 +1,12 @@
-﻿
+﻿import React from 'react';
+import {Route} from 'react-router';
+import App from './components/App';
+import Home from './components/Home';
+import AddAccount from './components/AddAccount';
+
+export default (
+    <Route component={App}>
+        <Route path='/' component={Home} />
+        <Route path='/add' component={AddAccount} />
+    </Route>
+);
